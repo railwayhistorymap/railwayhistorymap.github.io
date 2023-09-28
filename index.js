750feb5c4750b965c419176846ed6dbb6aa102a5
Alexey Gorshkov <agorshkov23@users.noreply.github.com>
2023-09-28 23:44:26 +0300

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+// config map
+let config = {};
+// magnification with which the map will start
+const zoom = 10;
+// co-ordinates
+const lat = 50;
+const lng = 30;
+
+// calling map
+const map = L.map("map", config).setView([lat, lng], zoom);
+
+// Used to load and display tile layers on the map
+// Most tile servers require attribution, which you can set under `Layer`
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
