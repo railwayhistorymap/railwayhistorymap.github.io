7ea8f02c761c262abeea20a5938244fc963244a0
Alexey Gorshkov <agorshkov23@mail.ru>
2023-09-28 22:48:25 +0300

    make package

--- build.ts
+++ build.ts
@@ -1,1 +1,7 @@
-console.log("Hello!")
+import * as fs from 'fs'
+
+for (const file of fs.readdirSync('.data')) {
+    if (file.endsWith('.geojson')) {
+        console.info(`found file: ${file}`)
+    }
+}
