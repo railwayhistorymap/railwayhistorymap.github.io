ebb0493ed82cde6c4438cba93ab9a10e202010a6
Alexey Gorshkov <agorshkov23@users.noreply.github.com>
2023-09-29 00:37:06 +0300

    update tile source

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ const lng = 30;
 
 // calling map
 const map = L.map("map", config).setView([lat, lng], zoom);
-map.attributionControl = false;
+
 
 // Used to load and display tile layers on the map
 // Most tile servers require attribution, which you can set under `Layer`
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+L.tileLayer(
+    // "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    "https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png",
+    {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+).addTo(map);
 
 fetch('/data.json')
     .then(response => response.json())
