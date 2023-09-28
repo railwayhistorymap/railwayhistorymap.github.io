0f8b26b65a90746c1c526e062fc0356422341b51
Alexey Gorshkov <agorshkov23@users.noreply.github.com>
2023-09-28 23:15:02 +0300

    Update build.ts

--- build.ts
+++ build.ts
@@ -3,8 +3,12 @@ import * as fs from 'fs'
 fs.mkdirSync("_site")
 fs.copyFileSync("index.html", "_site/index.html")
 
+const result = []
 for (const file of fs.readdirSync('.data')) {
     if (file.endsWith('.geojson')) {
         console.info(`found file: ${file}`)
+        const json = JSON.parse(fs.readFileSync(`.data/${file}`, {encoding: "utf8"}))
+        result.push(json)
     }
 }
+fs.writeFileSync('_site/data.json’, JSON.stringify(result))
