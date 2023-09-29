4aa0c8b34b1eb2a5d642baaecdbcace55142adce
Alexey Gorshkov <agorshkov23@mail.ru>
2023-09-29 23:13:25 +0300

    update 03

--- build.ts
+++ build.ts
@@ -1,8 +1,5 @@
 import * as fs from 'fs'
 
-fs.copyFileSync("index.html", "dist/index.html")
-fs.copyFileSync("index.js", "dist/index.js")
-
 const result = []
 for (const file of fs.readdirSync('.data')) {
     if (file.endsWith('.geojson')) {
