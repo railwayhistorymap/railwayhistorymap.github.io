e3ea63dfa8c6f4456df77d12d4280500828c7852
Alexey Gorshkov <agorshkov23@mail.ru>
2023-09-30 00:20:52 +0300

    update attributions

--- index.ts
+++ index.ts
@@ -1,33 +1,20 @@
-// config map
 let config = {};
-// magnification with which the map will start
 const zoom = 10;
-// co-ordinates
 const lat = 60;
 const lng = 30;
 
-
-// calling map
 const map = L.map("map",
     {
         attributionControl: false,
     }
 ).setView([lat, lng], zoom);
 
-const attributions = [
-    '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>',
-    'Рендеринг: <a href="https://openrailwaymap.org">OpenRailwayMap</a>, Данные карты &copy; участники OpenStreetMap',
-    '&copy; Алексей Горшков'
-];
-
-const attributionControl = L.control.attribution({
-    prefix: attributions.join(' | ')
-    // prefix: '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>'
-}).addTo(map);
+const attributionControl = L.control.attribution().addTo(map);
 
 L.tileLayer(
     "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
     {
+        attribution: '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>',
         opacity: 0.5
     }
 ).addTo(map);
@@ -36,8 +23,8 @@ L.tileLayer(
     // ,
     "https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png",
     {
+        attribution: 'Рендеринг: <a href="https://openrailwaymap.org">OpenRailwayMap</a>, Данные карты &copy; участники OpenStreetMap',
         opacity: 0.5,
-        //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }
 ).addTo(map);
 
@@ -45,7 +32,9 @@ fetch('/data.json')
     .then(response => response.json())
     .then(data => {
         for (const geojson of data) {
-            L.geoJSON(geojson, {}).addTo(map);
+            L.geoJSON(geojson, {
+                attribution: '&copy; Алексей Горшков'
+            }).addTo(map);
         }
     })
     .catch(e => {
@@ -56,4 +45,3 @@ fetch('/data.json')
 if (module['hot']) {
     module['hot'].accept();
 }
-
